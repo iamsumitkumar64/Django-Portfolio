ee482d7cd8df46213ee0c121b3cbe1587898ab91
Sumit Kumar <iam.sumitkumar64@gmail.com>
2025-03-16 14:06:13 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -3,7 +3,7 @@ pip3 install --upgrade pip
 pip3 install -r requirements.txt
 
 # Collect static files
-python3 manage.py collectstatic --noinput
+# python3 manage.py collectstatic --noinput
 
 # Run database migrations
 python3 manage.py makemigrations --noinput
